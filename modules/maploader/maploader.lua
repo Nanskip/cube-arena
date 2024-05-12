7c4684b55e9f2913dc21949d28b832e0b47d9053
Nanskip <nanskip.oof@gmail.com>
2024-05-12 17:47:53 +0300

    update maploader

--- modules/maploader/maploader.lua
+++ modules/maploader/maploader.lua
@@ -1,3 +1,36 @@
 maploader = {}
 
+maps = HTTP:Get("https://raw.githubusercontent.com/Nanskipp/cube-arena/main/data/maps.json", function(res)
+    if res.StatusCode ~= 200 then
+        error("Error on loading maps. Code: " .. res.StatusCode)
+        return
+    end
+    
+    return JSON:Decode(res.Body)
+end)
+
+load = function(map)
+    if map ~= maps.empty and map ~= maps.grove then
+
+    end
+    for x=1, #map do
+        for y=1, #map[x] do
+            local block = Block(Color(255, 255, 255), Number3(x, 0, y))
+            if map[x][y] == 0 then
+                block.Color = Color(235, 200, 106)
+            elseif map[x][y] == 1 then
+                block.Color = Color(217, 183, 93)
+            elseif map[x][y] == 2 then
+                block.Color = Color(101, 172, 219)
+            elseif map[x][y] == 3 then
+                block.Color = Color(235, 200, 106)
+            end
+
+            local getblock = Map:GetBlock(Number3(x, 0, y))
+            if getblock ~= nil then getblock:Remove() end
+            Map:AddBlock(block)
+        end
+    end
+end
+
 return maploader
