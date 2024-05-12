00b69e6a275f9fbc2889c68dd3971f336bd74146
Nanskip <nanskip.oof@gmail.com>
2024-05-12 13:31:40 +0300

    initial commit

---
+++ main.lua
@@ -0,0 +1,17 @@
+Config = {
+    Map = "nanskip.v",
+    Items = "nanskip.ca_bullet"
+}
+
+Modules = {
+    maploader = "github.com/Nanskipp/cube-arena/modules/maploader",
+    player = "github.com/Nanskipp/cube-arena/modules/player",
+}
+
+Client.OnStart = function()
+    
+end
+
+Client.Tick = function(dt)
+    
+end
