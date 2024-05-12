0b127ab0789624207031249c1fa451536d1b97d5
Nanskip <nanskip.oof@gmail.com>
2024-05-12 17:51:34 +0300

    fix2

--- modules/maploader/maploader.lua
+++ modules/maploader/maploader.lua
@@ -1,13 +1,15 @@
 maploader = {}
 
-maploader.loadMaps = HTTP:Get("https://raw.githubusercontent.com/Nanskipp/cube-arena/main/data/maps.json", function(res)
-    if res.StatusCode ~= 200 then
-        error("Error on loading maps. Code: " .. res.StatusCode)
-        return
-    end
-    
-    return JSON:Decode(res.Body)
-end)
+maploader.loadMaps = function()
+    maps = HTTP:Get("https://raw.githubusercontent.com/Nanskipp/cube-arena/main/data/maps.json", function(res)
+        if res.StatusCode ~= 200 then
+            error("Error on loading maps. Code: " .. res.StatusCode)
+            return
+        end
+        
+        return JSON:Decode(res.Body)
+    end)
+end
 
 maploader.load = function(map)
     if map ~= maps.empty and map ~= maps.grove then
